1200ec14e9e0ec6b27033f59778c7c4dd6690ca4
Julian Squires <julian.squires@adgear.com>
2015-09-03 10:57:12 -0400

    Change number of divisions in index to 2^14

--- c_src/indexed_ewah.h
+++ c_src/indexed_ewah.h
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include "ewok.h"
 
-enum { N_DIVISIONS = 65536, LOG2_DIVISIONS = 16 };
+enum { N_DIVISIONS = 1<<14 };
 struct indexed_ewah_map {
     struct ewah_bitmap *map;
     size_t bit_from_division[N_DIVISIONS], ptr_from_division[N_DIVISIONS];
